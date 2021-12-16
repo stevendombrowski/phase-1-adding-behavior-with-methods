09b1045205e2e998aec07ef17b018be298f42047
Steven Dombrowski <dombrowskis153@yahoo.com>
2021-12-16 01:06:37 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+class Dog {
+    constructor(name, gender){
+        this.name = name;
+        this.gender = gender;
+        this.speak = function(){
+            return `${this.name} says woof!`
+        }
+    }
+}
+
+class Cat{
+    constructor(name, gender){
+        this.name = name;
+        this.gender = gender;
+        this.speak = function(){
+            return `${this.name} says meow!`
+        }
+    }
+}
+
+class Bird{
+    constructor(name, gender){
+        this.name = name;
+        this.gender = gender;
+        this.speak = function(){
+            if (this.gender ==='male')
+            return `It's me! ${this.name}, the parrot!`
+            else if (this.gender != 'male')
+        return `${this.name} says squawk!`
+        
+    } 
+    }
+}
